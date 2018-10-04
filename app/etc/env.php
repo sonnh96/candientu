72db4aec5f7b82fce08e67d880b17ef6d5cf6ed4
sonnh96 <sonnh9966@gmail.com>
2018-10-04 09:29:33 +0700

    First

---
+++ app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_123',
+  ),
+  'crypt' => 
+  array (
+    'key' => '07f10efdb6f8f5522bca79fd6b06559d',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento',
+        'username' => 'root',
+        'password' => '1',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'production',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Mon, 24 Sep 2018 04:01:04 +0000',
+  ),
+);
